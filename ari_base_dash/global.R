d306f2098a29dedb53e18cca2518d5ccf69c4f4a
Daniel Chen <Maveri2k@yahoo.com>
2017-08-09 12:19:39 -0400

    links to data rel to dash code not prj

--- ari_base_dash/global.R
+++ ari_base_dash/global.R
@@ -1,8 +1,8 @@
 library(rio)
 library(dplyr)
 
-df <- read.csv('ari_base_dash/data/server_data/ACS_data_base.csv')
-data <- rio::import('ari_base_dash/data/server_data/test_map.csv')
-base_df <- rio::import('ari_base_dash/data/server_data/base_df.csv')
-county <- read.csv('ari_base_dash/data/server_data/ACS_data_county.csv')
-df_big <- read.csv('ari_base_dash/data/server_data/ACS_data_zctas.csv')
+df <- read.csv('data/server_data/ACS_data_base.csv')
+data <- rio::import('data/server_data/test_map.csv')
+base_df <- rio::import('data/server_data/base_df.csv')
+county <- read.csv('data/server_data/ACS_data_county.csv')
+df_big <- read.csv('data/server_data/ACS_data_zctas.csv')
